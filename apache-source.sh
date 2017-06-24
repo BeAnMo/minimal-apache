25426704666baa5bba4b472da78de800f5709dec
beanmo <bamorin00@gmail.com>
2017-06-24 16:30:41 -0500

    situation explained in comments

--- apache-source.sh
+++ apache-source.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+# courtesy of netnea.com's Apache compiling tutorial by Christian Folini
+# https://www.netnea.com/cms/apache-tutorial-1_compiling-apache/
+
 # RUN WITH SUDO, NO WAIT DON'T
+
+# make a source code dir: /usr/src/apache
+# chown of source dir to user/whoami
+# run script from source dir
+
+# libtoolT issue:
+# After configuring the source code, 'make' will stop with errors
+# relating to 'htpasswd' and undefined 'XML_' methods.
+# The following modifications seem to solve these problems.
+
+# on line 30000 or so of ./apr-1.6.2/configure file:
+# change:
+#	trap "$RM \"$cfgfile\"; ... (rest of line is fine)
+# to:
+#	trap "$RM -f \"$cfgfile\"; ...
+# on the line below, change:
+# 	$RM "$cfgfile"
+# to:
+# 	$RM -f "$cfgfile"	
+
+# remember to 'f'
 
 # cannot configure/build Apache without these
 printf '\nDownloading needed libraries\n'
